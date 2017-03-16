5b00f25e49af33cfde496451d60646eb360aeb64
Wojciech Bach <Wojciech Bach>
2017-03-16 20:43:19 +0100

    Remove logs, grass changes in progress

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "../GameEngine/Engine/Engine.h"
 #include "../GameEngine/Scene/Scene.hpp"
+#include "../GameEngine/Objects/RenderAble/Grass.h"
 #include "../GameEngine/Camera/FirstPersonCamera.h"
 #include "../GameEngine/Camera/ThridPersonCamera.h"
 #include "../GameEngine/Objects/RenderAble/Terrain.h"
@@ -64,6 +65,7 @@ void TestGameStart()
 		TestScene()
 		{
 		}
+
 		virtual int		Initialize()
 		{
 			auto crate = AddGameObject(new CEntity(m_ResourceManager, "../Data/Meshes/Crate/crate.obj", glm::vec3(0, 2, 0)), glm::vec3(0,0, -5));
@@ -88,20 +90,55 @@ void TestGameStart()
 			m_ResourceManager.GetTextureLaoder().ReadFile("../Data/Textures/heightmap.png", height_map, TextureFlip::VERTICAL);
 			terrain->LoadHeight(height_map);
 
+			std::vector<float> grass_position;
+			for(unsigned int y = 0 ; y < 100; y += 2)
+				for (unsigned int x = 0; x < 100; x += 2)
+				{
+					grass_position.push_back(x);
+					grass_position.push_back(terrain->GetHeightofTerrain(x, y));
+					grass_position.push_back(y);
+				}
+
+			//CGrass* grass = new CGrass();
+			//grass->model = new CModel();
+			//auto mesh = grass->model->AddMesh(grass_position, std::vector<float>(), std::vector<float>(), std::vector<float>(), std::vector<int>());
+			////mesh->
+			//m_ResourceManager.AddModel(grass->model);
+			//engine.m_Renderer->Subscribe(grass);
+			//m_Texture = scene->GetResourceManager().GetTextureLaoder().LoadTextureImmediately("../Data/Textures/G3_Nature_Plant_Grass_06_Diffuse_01.png");			
+	
+			/*grassvertex.resize(positions.size() * 3);
+			for (const auto& v : positions)
+			{
+				grassvertex.push_back(v.x);
+				grassvertex.push_back(v.y);
+				grassvertex.push_back(v.z);
+			}
+			m_MeshVao = Utils::CreateVao();
+			m_VertexSize = grassvertex.size() / 3;
+			m_VertexVbo = Utils::StoreDataInAttributesList(0, 3, grassvertex);
+			Utils::UnbindVao();*/
+
 			terrain->model = m_ResourceManager.LoadModel("../Data/Example/quad.obj");
 			m_ResourceManager.GetOpenGlLoader().AddObjectToOpenGLLoadingPass(terrain->model);
 
 			AddGameObject(terrain, glm::vec3(0.f, 0.f, 0.f));
 			engine.m_Renderer->Subscribe(terrain);
 
+
+
 		//	AddEntity("../Data/Meshes/Crate/crate.obj");
 			//AddEntity("../Data/Meshes/smallHouse1/smallHouse1.obj");
 		//	AddEntity("../Data/Meshes/G3_Myrtana_Castle_Mainhouse/G3_Myrtana_Castle_Mainhouse.obj");
 			//AddEntity("../Data/Meshes/fishingHouse/fishingHouse.obj");
 			//AddEntity("../Data/Meshes/alchemTable/alchemTable.obj");*/
-			//m_Camera = std::make_unique<CFirstPersonCamera>(&engine.m_InputManager, &engine.m_DisplayManager);
+			
+			m_DayNightCycle.SetDirectionalLight(&m_DirectionalLight);
+			m_DayNightCycle.SetTime(.5f);
 
-			m_Camera = std::make_unique<CThirdPersonCamera>(&engine.m_InputManager, player->m_WorldTransform);
+			m_Camera = std::make_unique<CFirstPersonCamera>(&engine.m_InputManager, &engine.m_DisplayManager);
+
+			//m_Camera = std::make_unique<CThirdPersonCamera>(&engine.m_InputManager, player->m_WorldTransform);
 			return 0;
 		}
 		virtual void	PostInitialize() {};
@@ -112,8 +149,14 @@ void TestGameStart()
 				m_Camera->CalculateInput();
 				m_Camera->Move();
 			}
+			m_DeltaTime = static_cast<float>(engine.m_DisplayManager.GetDeltaTime());
+			m_GloabalTime += m_DeltaTime;
 
-			player->Move(0.01f);
+			//std::cout << "Global Time : " << m_DayNightCycle.GetHours() << " : " << m_DayNightCycle.GetMinutes() << " : " << m_DayNightCycle.GetSeconds() << "\n";
+
+			m_DayNightCycle.Update(m_DeltaTime);
+
+			player->Move(m_DeltaTime);
 			player->CheckInputs();
 
 			auto height = terrain->GetHeightofTerrain(player->m_WorldTransform.GetPositionXZ());
@@ -123,9 +166,8 @@ void TestGameStart()
 				ppos.y = height;
 				player->SetPosition(ppos);
 			}
-
 			return 0;
-		}
+		}		
 	};
 	engine.Init();
 
@@ -138,6 +180,8 @@ void TestGameStart()
 
 int main(int argc, char* argv[])
 {	
+
+	
 	//MemoryTest();
 	CLogger::Instance().EnableLogs();
 	TestGameStart();
